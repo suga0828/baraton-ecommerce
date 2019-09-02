ff7be3ff63d9e54e4d2dc3adb0aab9814a55947a
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-02 13:24:42 -0500

    Load HomeComponent, ProductsComponent and ShoppingCart component to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
