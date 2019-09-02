5e9be4a677a3724464cd32a5272eada698ab8cf6
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-02 13:05:39 -0500

    Import Material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,31 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+import{ MatButtonModule } from '@angular/material/button';
+import{ MatToolbarModule } from '@angular/material/toolbar';
+import{ MatGridListModule } from '@angular/material/grid-list';
+import{ MatSidenavModule } from '@angular/material/sidenav';
+import{ MatCardModule } from '@angular/material/card';
+import{ MatTreeModule } from '@angular/material/tree';
+import{ MatCheckboxModule } from '@angular/material/checkbox';
+import{ MatSliderModule } from '@angular/material/slider';
+import{ MatSnackBarModule } from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatSnackBarModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, MatButtonModule, MatToolbarModule, MatGridListModule, MatSidenavModule, MatCardModule, MatTreeModule, MatCheckboxModule, MatSliderModule, MatSnackBarModule]
 })
 export class SharedModule { }
