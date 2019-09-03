98a1444f372b4b933b165d4b845d620c5dc05104
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-02 19:57:04 -0500

    Load Material Icons and Browser animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
   {
     path: 'landing',
     loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
